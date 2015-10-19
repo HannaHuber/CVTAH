41c9d197a58893b56333fa52119bb659221865e8
HannaHuber <hanna_huber@hotmail.com>
2015-10-19 15:07:56 +0200

    Fix bugs in for-loop and string concat

--- UE1/A1/main.m
+++ UE1/A1/main.m
@@ -13,23 +13,26 @@ shift = [i(:),j(:)];
 % Maximum correlation
 max_corr_rg = -1;
 max_corr_rb = -1;
-for k = size(shift, 1)
+for k = 1:size(shift, 1)
     l = shift(k, 1:2);
     g_shifted = circshift(g, l);
     b_shifted = circshift(b, l);
     ncc_rg = corr2(r, g_shifted);
-    ncc_rb = coor2(r, b_shifted);
-    if (abs(ncc_rg) > max_corr_rg )
-        max_corr_rg = abs(ncc_rg);
-        g_shifted_correct = g_correct;
+    ncc_rb = corr2(r, b_shifted);
+    if (ncc_rg > max_corr_rg )
+        max_corr_rg = ncc_rg;
+        g_shifted_correct = g_shifted;
     end
-    if (abs(ncc_rb) > max_corr_rb )
-        max_corr_rb = abs(ncc_rb);
-        b_shifted_correct = b_correct;
+    if (ncc_rb > max_corr_rb )
+        max_corr_rb = ncc_rb;
+        b_shifted_correct = b_shifted;
     end    
 end
 %% Create RGB image
-rgb = [r,g_shifted_correct, b_shifted_correct];
-filename = strcat(strsplit(img1_r, '_'),'_RGB.jpg');
-imwrite(rgb, filename, 'jpg');
+rgb = cat(3,r,g_shifted_correct, b_shifted_correct);
+original_name = strsplit(img_r, '_');
+filename = strcat(original_name(1), '_RGB.jpg');
+filename = strjoin(filename);
+imshow(rgb);
+imwrite(rgb, filename);
 end

e5346e233e29d92da985fd45c653b5c586e76ab5
HannaHuber <hanna_huber@hotmail.com>
2015-10-27 11:30:53 +0100

    add stuff to main.m
    - input k : number of clusters
    - comments
    - init J

--- UE1/A2/main.m
+++ UE1/A2/main.m
@@ -1,33 +1,38 @@
 %% Assignment I/2: Image Segmentation by K-means Clustering
 % Authors: Tomas Musil, Andreas Wittmann, Hanna Huber
 
-function [] = main(img_path, d3_or_d5, cluster_treshold)
+function [] = main(img_path, d3_or_d5, k, cluster_treshold)
 % Input: 
-%   img_path  ... path to image (which should be clustered)
-%   3D_or_5D  ... '3D' if it should only use color information for
-%   clustering, '5D' if it should use color and spatial information for
-%   clustering
-%   cluster_treshold ...if the J ratio lies under a given threshold, terminate the clustering
+%   img_path            ... path to image (which should be clustered)
+%   3D_or_5D            ... '3D' if it should only use color information for
+%                           clustering, '5D' if it should use color and spatial information for
+%                           clustering
+%   k                   ... number of clusters
+%   cluster_treshold    ... if the J ratio reaches a given threshold, terminate the clustering
 %
-% Output: (saved): clustered ...clustered image with colorized cluster
-% pixels (mean color values of cluster pixels)
-
+% Output (saved): 
+%   clustered           ... clustered image with colorized cluster
+%                           pixels (mean color values of cluster pixels)
+%% Read images
+% TODO
+%% Calculate data points
 Xn = calc_Xn(img_path, d3_or_d5);
-
-%init J
-J = -1; %TODO sinnvollen Wert initialisieren
-J_ratio = -1; %TODO sinnvollen Wert initialisieren
+%% K-Means iteration
+% init J
+J = Inf(1);
+J_ratio = Inf(1);
 my_k = calc_rand_myk(size(Xn, 1));
-
-while(J_ratio < cluster_treshold)
+% iterate until treshold is reached
+while(J_ratio > cluster_treshold)
+    % assign data points to clusters
     r = calc_r(Xn, my_k);
+    % recalculate cluster centroids
     my_k = calc_cluster_centroids(r, Xn);
+    % calculate distortion measure
     J_new = calc_j(Xn, r, my_k);
-    
-    J_ratio = abs(J_new-J);
+    J_ratio = J/J_new;
     J = J_new;
 end
-
-%image segmentation
+%% Image segmentation
 %TODO
 end

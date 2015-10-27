2dae56eefb581cf1c5f1c7358309044a3bc0dd61
HannaHuber <hanna_huber@hotmail.com>
2015-10-27 11:29:50 +0100

    finish calc_j.m

--- UE1/A2/calc_j.m
+++ UE1/A2/calc_j.m
@@ -1,7 +1,21 @@
-function [ J ] = calc_j(Xn, r, my_k)
-%CALC_J Summary of this function goes here
-%   Detailed explanation goes here
-J = -1;
+function [ J ] = calc_j( xn, mk, r  )
+%CALC_J objective function for the k-means clustering algorithm
+%   Computes the sum of the squares of the distances of each data point to its assigned
+%   cluster centroid
+%   Input:  xn  ...     DxN Matrix of D-dimensional datapoints
+%           mk  ...     DxK Matrix cluster centroids
+%           r   ...     NxK Matrix assigning each datapoint a particular
+%                       cluster:
+%                       r(n,k) = 1 if xn belongs to cluster k
+%                       r(n,k) = 0 otherwise
+%   Output: J   ...     scalar representing the measure of distortion
+
+N = size(xn, 2);
+J = 0;
+
+for n = 1:N
+    J = J + norm(xn(:,n)- mk*(r(n,:))');
+end
 
 end
 

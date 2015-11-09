ce2bf64faedbd237894c53199342d1d09a5d7b8a
HannaHuber <hanna_huber@hotmail.com>
2015-11-09 13:21:46 +0100

    init my_k in calc_rand_myk

--- UE1/A2/calc_rand_myk.m
+++ UE1/A2/calc_rand_myk.m
@@ -6,6 +6,7 @@ function [ my_k ] = calc_rand_myk(d, k )
 %   my_k        ...     clusters with randomly initialized values between 0
 %                       and 1
 %% Iterate over whole matrix
+my_k = zeros(d,k);
 for i = 1:k
     for j = 1:d
         % Choose a random number between 0 and 1

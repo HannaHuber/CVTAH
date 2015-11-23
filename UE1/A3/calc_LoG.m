e4e3f163dd35ae658581f7ca66bffc09a2e6c0d1
Andreas Wittmann <andreas.wittann.aw@gmail.com>
2015-11-23 14:59:54 +0100

    comments

--- UE1/A3/calc_LoG.m
+++ UE1/A3/calc_LoG.m
@@ -8,6 +8,8 @@ function [ filter ] = calc_LoG( sigma )
 
 %% Build a scale-normalized Laplacian of Gaussian filter with current scale.
 % count filter size
+% A good choice is to restrict the filter size
+% to the range [-3sigma my; 3sigma my], i.e. the filter size is computed as 2|_3 my sigma_| + 1. 
 size = 2 * floor(3 * sigma) + 1;
 filterSize = [size size];
 
@@ -15,14 +17,16 @@ filterSize = [size size];
 filter = fspecial('log', filterSize, sigma);
 
 % Scale-normalizing the filter
+% Since the response of LoG decreases as my increases, you have to scale-normalize
+% the filter by multiplying it with my^2.
 filter =  sigma^2 * filter;
 
 %% Hint (see assignment)
 % Filter creation: use the fspecial function with parameter 'log' to create the LoG
-% filters. Since the response of LoG decreases as  increases, you have to scale-normalize
-% the filter by multiplying it with 2. In order to decrease computation time for smaller ,
-% you should set the filter size proportional to . A good choice is to restrict the filter size
-% to the range [-3sigma; 3sigma], i.e. the filter size is computed as 2|_3sigma_| + 1. 
+% filters. Since the response of LoG decreases as my increases, you have to scale-normalize
+% the filter by multiplying it with my^2. In order to decrease computation time for smaller my,
+% you should set the filter size proportional to my. A good choice is to restrict the filter size
+% to the range [-3sigma my; 3sigma my], i.e. the filter size is computed as 2|_3 my sigma_| + 1. 
 % Note: |_ and _| = Gaussklammer --> abrunden!
 end
 

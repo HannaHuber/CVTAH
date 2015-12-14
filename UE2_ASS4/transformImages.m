e3ff4f8281e410b041b7042263769291a15d255f
HannaHuber <hanna_huber@hotmail.com>
2015-12-14 15:17:04 +0100

    rm tranformImages.m

--- UE2_ASS4/transformImages.m
+++
@@ -1,9 +0,0 @@
-function transformed = transformImages(H,I, xy_min, xy_max)
-% TODO C.4
-n = size(I);
-transformed = cell(1,n);
-for i=1:n
-    transformed{i} = imtransform(I{i},H{i},'XData',[ xy_min(1) xy_max(1)], 'YData',[ xy_min(2) xy_max(2)]);
-end
-
-end
